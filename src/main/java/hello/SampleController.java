a44930994c9ee82ef92c5a403834bd374e3ccdbf
hengzizng <hengzizng@naver.com>
2018-09-28 16:08:52 +0900

    7조

--- src/main/java/hello/SampleController.java
+++ src/main/java/hello/SampleController.java
@@ -15,7 +15,7 @@ public class SampleController {
     @RequestMapping("/")
     @ResponseBody
     String home() {
-        return "Hello World! This is " + message ;
+        return "7조 Hello World! This is " + message ;
     }
 
     public static void main(String[] args) throws Exception {
